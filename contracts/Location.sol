f0461fe4054bd0a1e31d9bc9a20491fa3c2cb41a
0xLazard1 <DevLazard@proton.me>
2023-04-01 22:42:52 +0300

    MAJ2

--- contracts/Location.sol
+++ contracts/Location.sol
@@ -3,51 +3,74 @@ pragma solidity 0.8.19;
 
 contract Location {
     address public owner;
-    uint256 public NumberOfObject = 0;
-    string[2] Categorie = ["Technologie", "Livre"];
+    uint256 public numberOfObjects = 0;
+    string[2] categories = ["Technologie", "Livre"];
+    string[] lieux;
 
     struct Object {
         uint256 id;
-        string names;
-        string descriptions;
-        uint256 prices;
-        uint256 timesoflocations;
+        string name;
+        string description;
+        uint256 price;
+        uint256 timeOfLocation;
         address payable owner;
-        string categorie;
+        string category;
+        string lieu;
+        bool isAvailable;
     }
 
-    mapping(uint256 => Object) public Objects;
+    mapping(uint256 => Object) public objects;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function ObjectRegister(
-        string memory _names,
+    function registerObject(
+        string memory _name,
         string memory _description,
         uint256 _price,
         uint256 _timeOfLocation,
-        string memory _categorie
+        string memory _category,
+        string memory _lieu
     ) external {
-        require(msg.sender != address(0), "!Not people");
-        require(CategorieExit(_categorie), "!CategoerieExistepas");
-        NumberOfObject++;
+        require(msg.sender != address(0), "Invalid address");
+        require(categoryExists(_category), "Category does not exist");
 
-        Object storage object = Objects[NumberOfObject];
-        object.id = NumberOfObject;
-        object.names = _names;
-        object.descriptions = _description;
-        object.prices = _price;
-        object.timesoflocations = block.timestamp + _timeOfLocation;
+        if (!lieuExists(_lieu)) {
+            lieux.push(_lieu);
+        }
+
+        numberOfObjects++;
+
+        Object storage object = objects[numberOfObjects];
+        object.id = numberOfObjects;
+        object.name = _name;
+        object.description = _description;
+        object.price = _price;
+        object.timeOfLocation = block.timestamp + _timeOfLocation;
         object.owner = payable(msg.sender);
-        object.categorie = _categorie;
+        object.category = _category;
+        object.lieu = _lieu;
+        object.isAvailable = true;
     }
 
-    function CategorieExit(string memory categorie) public view returns (bool) {
-        for (uint256 i = 0; i < Categorie.length; i++) {
-            if (keccak256(bytes(Categorie[i])) == keccak256(bytes(categorie)))
+    function categoryExists(string memory category) public view returns (bool) {
+        for (uint256 i = 0; i < categories.length; i++) {
+            if (keccak256(bytes(categories[i])) == keccak256(bytes(category))) {
                 return true;
+            }
         }
         return false;
     }
+
+    function lieuExists(string memory lieu) public view returns (bool) {
+        for (uint256 i = 0; i < lieux.length; i++) {
+            if (keccak256(bytes(lieux[i])) == keccak256(bytes(lieu))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function ResearchObject(string memory category, string memory lieu) external view returns()
 }
--- test/Location.js
+++ test/Location.js
@@ -1,6 +1,9 @@
 const chai = require("chai");
+const { constants } = require("ethers");
 const expect = chai.expect;
 const { ethers } = require("hardhat");
+
+
 
 describe("Location Contract", function () {
     let Location;
@@ -17,55 +20,59 @@ describe("Location Contract", function () {
         await location.deployed();
     })
 
-    describe('Deployment', function () {
-        it("Should set owner equal to msg.sender", async function () {
+    describe("Deployment", function () {
+        it("Should be owner to equal at msg.sender", async function () {
             expect(await location.owner()).to.equal(owner.address);
         });
 
-        it("Should set NumberOfObject to 0", async function () {
-            expect(await location.NumberOfObject()).to.equal(0);
+        it("Should be numberofprojets equal at 0", async function () {
+            expect(await location.numberOfObjects()).to.equal(0);
         })
 
-        it("Should initialize Object with zero values", async function () {
-            const obj = await location.Objects(0);
+        it("Should be Object is not defined", async function () {
+            const obj = await location.objects(0);
             expect(obj.id).to.equal(0);
-            expect(obj.names).to.equal("");
-            expect(obj.descriptions).to.equal("");
-            expect(obj.prices).to.equal(0);
-            expect(obj.timesoflocations).to.equal(0);
+            expect(obj.name).to.equal("");
+            expect(obj.description).to.equal("");
+            expect(obj.price).to.equal(0);
+            expect(obj.timeOfLocation).to.equal(0);
             expect(obj.owner).to.equal(ethers.constants.AddressZero);
-            expect(obj.categorie).to.equal("");
+            expect(obj.category).to.equal("");
+            expect(obj.lieu).to.equal("");
+            expect(obj.isAvailable).to.equal(false);
         });
-    })
 
-    describe('CreateObject', function () {
-        it('Should add new object', async function () {
-
-            await location.connect(owner).ObjectRegister("Lampes", "Jolie Lampes", 1, 500, "Technologie");
-            expect(await location.NumberOfObject()).to.equal(1);
-
-            const obj = await location.Objects(1);
-            expect(obj.id).to.equal(1);
-            expect(obj.names).to.equal("Lampes");
-            expect(obj.descriptions).to.equal("Jolie Lampes");
-            expect(obj.prices).to.equal(1);
-            expect(obj.timesoflocations).to.be.closeTo(
-                (await ethers.provider.getBlock()).timestamp + 500, 2
-            );
-            expect(obj.owner).to.equal(owner.address);
-            expect(obj.categorie).to.equal("Technologie");
-        })
-    })
-
-    describe('CategorieExit', function () {
-        it('Should return true for an existing category', async function () {
-            const result = await location.CategorieExit("Technologie");
-            expect(result).to.equal(true);
+        describe("Registred a new object", function () {
+            it("Should create a new Object", async function () {
+                await location.connect(owner).registerObject("Velo", "Velo Simple", 1, 500, "Technologie", "Vilnius");
+                expect(await location.numberOfObjects()).to.equal(1);
+                const obj = await location.objects(1);
+                expect(obj.id).to.equal(1);
+                expect(obj.name).to.equal("Velo");
+                expect(obj.description).to.equal("Velo Simple");
+                expect(obj.price).to.equal(1);
+                expect(obj.timeOfLocation).to.be.closeTo(
+                    (await ethers.provider.getBlock()).timestamp + 500, 2
+                );
+                expect(obj.owner).to.equal(owner.address);
+                expect(obj.category).to.equal("Technologie");
+                expect(obj.lieu).to.equal("Vilnius");
+                expect(obj.isAvailable).to.equal(true);
+            });
         })
 
-        it('Should return false for a non-existing category', async function () {
-            const result = await location.CategorieExit("NonExistingCategory");
-            expect(result).to.equal(false);
+        describe("IsExists", function () {
+            it("Should says that CategorieExit", async function () {
+                const result = await location.categoryExists("Technologie");
+                expect(result).to.equal(true);
+            })
+
+            it("Should says don't CategorieExit", async function () {
+                const result = await location.categoryExists("");
+                expect(result).to.equal(false);
+            })
         })
-    })
+    });
+
+
 })
